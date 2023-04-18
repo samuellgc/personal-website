a8e893cd509cdc2f18fb74bb0b74b4d052549017
samuellgc <samuellgc@hotmail.com>
2023-04-18 17:08:39 -0300

    Creating hero content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from '@/components/sections/hero';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Projeto site aqui</p>
+    <main className="flex min-h-screen flex-col">
+      <Hero />
     </main>
   );
 }
---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <main className="flex w-full flex-col sm:justify-center sm:items-center md:h-screen">
+      <h2 className="text-2xl font-bold ">Dev. Samuel Carneiro</h2>
+      <p>Desenvolvedor full-stack com foco em Front-end e gestão de equipes.</p>
+    </main>
+  );
+}
