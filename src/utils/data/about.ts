ba7b8e2a0a3b1efd67ba22c0b0aa45e07048b116
samuellgc <samuellgc@hotmail.com>
2023-04-22 17:05:03 -0300

    Few adjustments

--- src/utils/data/about.ts
+++ src/utils/data/about.ts
@@ -1,10 +1,10 @@
 export const aboutPtBr = `
-Eu não sou apenas um desenvolvedor, eu sou desenvolvedor, gestor e um visionário.
-Sou formado em Processos Gerenciais e Administração e já atuei em diversas posições de carreira, como estagiário financeiro, em processos e projetos, 
+Além de atuar como um desenvolvedor, também atuo como gestor e sou um visionário.
+Me formei em Processos Gerenciais e Administração e atuei em diversas posições de carreira, como estagiário financeiro, em processos e projetos, 
 além disso, atuei como assistente de controladoria em um dos maiores mercados do Ceará. 
-Sempre fui ligado em tecnologia e queria que meus esforços pudessem atingir diversas pessoas, 
-então comecei a aprender e estudar programação, desde então tive uma grande evolução de carreira e de vida e atualmente estou atuando como engenheiro de software em uma empresa da Dinamarca. 
-Pra mim estudar e aprender é um hobbie e venho me aperfeiçoando nos assuntos que mais me atraem, como 
+Sempre fui ligado em tecnologia e almejo que meus esforços possam atingir o maior número de pessoas, 
+então comecei a aprender e estudar programação, desde então tive uma grande evolução de carreira e de vida e atualmente estou atuando como desenvolvedor de software em uma empresa da Dinamarca. 
+Para mim, estudar e aprender é um hobbie e venho me aperfeiçoando nos assuntos que mais me atraem, como 
 programação, liderança, filosofia, astronomia, entre outros temas... E meu grande objetivo é poder influenciar pessoas e 
 ajudá-las ao máximo possível com meus projetos e trabalhos.
 `;
